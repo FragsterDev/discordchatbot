2a1c5436fb82cfec80b0896b8f31f0fe936354ef
Fragster Dev <72210275+FragsterDev@users.noreply.github.com>
2022-04-02 12:03:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import discord
+from discord.ext import commands
+from webserver import keep_alive
+
+intents = discord.Intents(messages=True, guilds=True)
+
+admin_id = 505814544002842644
+prefix = "+"
+client = commands.Bot(command_prefix=commands.when_mentioned_or("+"),intents=intents)
+
+@client.command()
+async def load(ctx, extension):
+  if ctx.author.id == admin_id:
+    client.load_extension(f"cogs.{extension}")
+    await ctx.send(f"**Successfully loaded:** `{extension}` !")
+  else:
+    await ctx.send("**Error**: Only bot admins can use this command.")
+
+@client.command()
+async def unload(ctx, extension):
+  if ctx.author.id == admin_id:
+    client.unload_extension(f"cogs.{extension}")
+    await ctx.send(f"**Successfully unloaded:** `{extension}` !")
+  else:
+    await ctx.send("**Error**: Only bot admins can use this command.")
+
+@client.command()
+async def reload(ctx, extension):
+  if ctx.author.id == admin_id:
+    client.unload_extension(f"cogs.{extension}")
+    client.load_extension(f"cogs.{extension}")
+    await ctx.send(f"**Successfully reloaded:** `{extension}` !")
+  else:
+    await ctx.send("**Error**: Only bot admins can use this command.")
+
+for filename in os.listdir('./cogs'):
+  if filename.endswith('.py'):
+    client.load_extension(f"cogs.{filename[:-3]}")
+    print(f"Successfully loaded {filename}\n")
+
+
+
+
+keep_alive()
+client.run(os.getenv('TOKEN'))
